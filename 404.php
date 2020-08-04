ac1c10a26b95d8d3ea91379bb62117e6d084397a
Oswald Prince <34043599+oswaldpr@users.noreply.github.com>
2020-08-04 08:49:55 -0400

    404

---
+++ 404.php
@@ -0,0 +1,9 @@
+<video class="video media" id="video-wateryadorablecrow"
+       alt="404"
+       height="772" width="1576" autoplay="" playsinline="" preload="auto"
+       poster="https://thumbs.gfycat.com/WateryAdorableCrow-poster.jpg" tabindex="-1"
+       style="max-width: 1576px; margin: 0px auto; display: block;">
+    <source src="https://giant.gfycat.com/WateryAdorableCrow.webm" type="video/webm">
+    <source src="https://giant.gfycat.com/WateryAdorableCrow.mp4" type="video/mp4">
+    <source src="https://thumbs.gfycat.com/WateryAdorableCrow-mobile.mp4" type="video/mp4">
+</video>
